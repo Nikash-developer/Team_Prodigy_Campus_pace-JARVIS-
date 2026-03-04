79662382fae2ac66b2a48bd2a93f12e724446b72
Nikash Jaipal <nikashjaipal2008@gmail.com>
2026-03-04 22:51:42 +0530

    diagnostic: test express import

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
+import express from 'express';
+
 export default function handler(req: any, res: any) {
-    res.status(200).send("Pure function is working!");
+    res.status(200).send("Express imported successfully!");
 }
